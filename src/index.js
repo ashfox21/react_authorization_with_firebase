2ef4908762ce2feb18eedfc89db3b22e5af53774
Ashfox21 <ashfox201@gmail.com>
2024-01-18 16:50:18 +0200

    connect firebase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import {store} from './store'
+import {store} from './store';
+import './index.css';
+import './firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
